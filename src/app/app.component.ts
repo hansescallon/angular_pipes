9bea350d0733fb65715ab165f64528774bf23f8c
hescallon <hr_escallon@hotmail.com>
2024-07-17 11:06:26 -0500

    ejemplo de uso de pipes en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre:string = 'Capitan America';
+  arreglo:number[] = [1,2,3,4,5,6,7,8,9,10];
+  PI:number = 3.141516;
+  porcentaje: number = 0.234;
+  salario:number = 1234.5;
+  fecha :Date = new Date();
+  url: string = 'https://www.youtube.com/embed/TZAklerD988?si=V3Hijf6-p2zItXVB';
+  heroe = {
+    nombre:'Logan',
+    clave:'wolverine',
+    edad:500,
+    direccion : {
+      calle: 'primera',
+      casa:20
+    }
+  }
 }
